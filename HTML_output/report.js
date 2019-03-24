bc2615a9189b39f1e13ff922ffcdf7db06f2cf2c
pradee246 <35226664+pradee246@users.noreply.github.com>
2019-03-24 15:12:12 +0530

    Add files via upload

---
+++ HTML_output/report.js
@@ -0,0 +1,45 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("D:/@Testing/WS2018/Learn/src/main/java/feature/deals.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Deal data creation",
+  "description": "",
+  "id": "deal-data-creation",
+  "keyword": "Feature"
+});
+formatter.before({
+  "duration": 405485,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 319637,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 3,
+  "name": "Free CRM Create a new deal scenario",
+  "description": "",
+  "id": "deal-data-creation;free-crm-create-a-new-deal-scenario",
+  "type": "scenario",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "line": 5,
+  "name": "User is in Login page",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "LoginScenario.user_is_in_Login_page()"
+});
+formatter.result({
+  "duration": 21932043086,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 177096,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 239188,
+  "status": "passed"
+});
+});
